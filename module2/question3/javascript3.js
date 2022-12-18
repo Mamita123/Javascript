55a30dd9a5ae96096b9623481ac131d0167eb18b
Mamita123 <mamitagurung895@gmail.com>
2022-12-18 11:35:34 +0200

    Added module2

---
+++ module2/question3/javascript3.js
@@ -0,0 +1,16 @@
+/* Write a program that asks for the names of six dogs. The program prints dog names to unordered list <ul> in reverse alphabetical order.*/
+
+let dog_name = [];
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 6; i++){
+  dog_name.push(prompt(`Enter the dog name:`));
+}
+
+dog_name.reverse();
+
+for (let i = 0; i < 6; i++){
+  ul.innerHTML += `<li>${dog_name[i]}</li>`;
+}
+document.body.appendChild(ul);
